c6614b18e73c13617438c02158da24f480dec988
galiciaabraham <galicia.abraham@hotmail.com>
2023-04-20 20:00:05 -0500

    corrected copyright sign

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,5 @@
 let year = new Date().getFullYear();
 
-document.querySelector('#copyright').textContent = `${year} .:|:. Abraham Galicia .:|:. Mexico`;
+document.querySelector('#copyright-year').textContent = year;
 
 document.querySelector('#last-updated').textContent = `Last modified: ${document.lastModified}`;
