5865690b4d51af504da4e9d78074d624683a573f
galiciaabraham <galicia.abraham@hotmail.com>
2023-05-11 17:39:24 -0500

    finished x on hamburguer exercise

--- start-navigation/start-navigation/js/scripts.js
+++ start-navigation/start-navigation/js/scripts.js
@@ -1,6 +1,8 @@
 function toggleMenu() {
     document.getElementById('primary-nav').classList.toggle('open');
+    document.getElementById('hamburger').classList.toggle('open');
 }
+
 
 const x = document.getElementById('hamburger')
 x.onclick = toggleMenu;
