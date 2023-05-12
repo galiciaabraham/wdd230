126a920a86ca3a7887dd244c4342cb7719a628df
galiciaabraham <galicia.abraham@hotmail.com>
2023-05-12 11:20:40 -0500

    started adding the today code in js file

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,22 @@
-let year = new Date().getFullYear();
+const today = new Date();
+
+let year = today.getFullYear();
+let month = today.getMonth();
+let day = today.getDay();
+
+function monthInText() {
+    let monthText = "";
+    switch(month){
+        case 0 :
+             monthText = "January";
+             break;
+    }
+}
+
+function todayIs() {
+    document.querySelector('#today').textContent = `${}`;
+}
+
 
 document.querySelector('#copyright-year').textContent = year;
 
