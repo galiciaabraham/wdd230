19a5b55156ece5cfad0422db662ac5fe6e536297
galiciaabraham <galicia.abraham@hotmail.com>
2023-05-16 10:20:46 -0500

    dom exercise

--- dom-exercise-week-5/scripts/scripts.js
+++ dom-exercise-week-5/scripts/scripts.js
@@ -1,8 +1,21 @@
-function toggleMenu() {
-    document.getElementById('primary-nav').classList.toggle('open');
-    document.getElementById('hamburger').classList.toggle('open');
-}
+//Change elements using DOM
+const link = document.querySelector("a");
+link.textContent = "Mozilla Developer Network";
+link.href = "https://developer.mozilla.org";
+//Add elements using DOM
+const sect = document.querySelector("section");
+const para = document.createElement("p");
+para.textContent = "We hope you enjoyed the ride";
+sect.appendChild(para);
+//Add text to an element using DOM
+const text = document.createTextNode(
+    " - the premier source for web development knowledge"
+);
+const linkPara = document.querySelector("p");
+linkPara.appendChild(text);
+//Move or remove nodes using DOM.
+sect.appendChild(linkPara);//It moves the referenced copy to the bottom of the page. 
+//const copyNode = sect.cloneNode(linkPara);//It makes a copy of the element and appends it to the end of the element. sect.appendChild(copyNode);
 
+sect.removeChild(linkPara);
 
-const x = document.getElementById('hamburger')
-x.onclick = toggleMenu;
