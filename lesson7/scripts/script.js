4e6a2a606284fdb02b6e69baac3e6d94f4380ced
galiciaabraham <galicia.abraham@hotmail.com>
2023-05-31 07:01:01 -0500

    finished lazy loading activity

---
+++ lesson7/scripts/script.js
@@ -0,0 +1,29 @@
+let imagesToLoad = document.querySelectorAll("img[data-src]");
+
+const loadImages = (image) => {
+    image.setAttribute("src", image.getAttribute("data-src"));
+    image.onload = () => {
+        image.removeAttribute("data-src");
+    };
+};
+
+const imgOptions = {
+    threshold: 0,
+    rootMargin: "0px 0px 100px 0px"
+};
+
+
+const imgObserver = new IntersectionObserver((items, imgObserver) => {
+        items.forEach(item => {
+            if (!item.isIntersecting) {
+                return; 
+            } else { 
+                loadImages(item.target);
+                imgObserver.unobserve(item.target);
+            }
+        });
+    }, imgOptions); 
+
+    imagesToLoad.forEach(image => {
+        imgObserver.observe(image);
+    })
