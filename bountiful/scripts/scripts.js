72a984c100739968e788938fa7bce3563e4b84b5
galiciaabraham <galicia.abraham@hotmail.com>
2023-07-08 18:33:59 -0500

    need to fix hamburguer position

--- bountiful/scripts/scripts.js
+++ bountiful/scripts/scripts.js
@@ -1,25 +1,9 @@
 function toggleMenu() {
-    document.getElementById('primary-nav').classList.toggle('open');
-    document.getElementById('hamburger').classList.toggle('open');
+    document.querySelector('.home-nav').classList.toggle('open');
+    document.querySelector('#hamburger').classList.toggle('open');
 }
 
 
-const x = document.getElementById('hamburger')
-x.onclick = toggleMenu;
-
-const button = document.querySelector('.close');
-const banner = document.querySelector('.banner')
-
-button.addEventListener('click', ()=>{
-    banner.parentNode.removeChild(banner);
-});
-
-let today = new Date().getDay();
-
-if (today == 1 || today == 2){
-    banner.setAttribute('class','banner');
-} else {
-    banner.setAttribute('class','no-tuesday');
-
-}
+const toggleClick = document.querySelector('#hamburger');
+toggleClick.onclick = toggleMenu;
 
